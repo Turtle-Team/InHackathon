f9bc802a31d08105dee3c29d4f771175d6c036e4
igeg9 <igeg91999@gmail.com>
2022-12-15 20:28:35 +0300

    Добавление экрана авторизации, регистрации, мероприятия, участвую. Добавлена навигация между экранами, панель навигации. И т.д.

---
+++ app/src/main/java/com/turtleteam/myapp/ui/fragments/register/RegisterFragment.kt
@@ -0,0 +1,29 @@
+package com.turtleteam.myapp.ui.fragments.register
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.turtleteam.myapp.R
+import com.turtleteam.myapp.databinding.FragmentRegisterBinding
+
+class RegisterFragment : Fragment() {
+
+    private lateinit var binding: FragmentRegisterBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentRegisterBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.registerButton.setOnClickListener {
+            findNavController().navigate(R.id.action_registerFragment_to_homeFragment)
+        }
+    }
+}
